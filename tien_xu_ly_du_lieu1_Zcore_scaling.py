0717f6badc5bbb577cd536dff20e049844991788
thucbui86 <105800480+thucbui86@users.noreply.github.com>
2022-09-13 22:17:33 +0700

    Add files via upload

---
+++ tien_xu_ly_du_lieu1_Zcore_scaling.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+
+# tạo các cột theo các phần phối khác nhau
+df = pd.DataFrame({ 
+    'beta': np.random.beta(5, 1, 1000) * 60,        # beta
+    'exponential': np.random.exponential(10, 1000), # exponential
+    'normal_p': np.random.normal(10, 2, 1000),      # normal platykurtic
+    'normal_l': np.random.normal(10, 10, 1000),     # normal leptokurtic
+})
+
+# thêm dữ liệu được tạo theo phân phối nhị thức
+first_half = np.random.normal(20, 3, 500) 
+second_half = np.random.normal(-20, 3, 500) 
+bimodal = np.concatenate([first_half, second_half])
+
+df['bimodal'] = bimodal
+
+# hiển thị dữ liệu
+print(df.head())
+
+# 1.Trực quan hóa dữ liệu sinh ra
+print(sns.kdeplot(data=df))
+
+# 2. Hiển thị thống kê về dữ liệu sinh ra
+print(df.describe())
+
+# 3. Thêm một đặc trưng với giá trị lớn hơn nhiều
+normal_big = np.random.normal(1000000, 10000, (1000,1))  # normal distribution of large values
+df['normal_big'] = normal_big
+print(sns.kdeplot(data=df))
+
+# trực quan hóa bằng biểu đồ box plot
+df.boxplot()
+
+# Khai báo đối tượng StandardScaler
+s_scaler = StandardScaler()
+
+# Chuẩn hóa dữ liệu trong df với StandardScaler
+df_s = s_scaler.fit_transform(df)
+
+# lấy danh sáchc cột
+col_names = list(df.columns)
+
+# chuyển về DataFrame, gán các cột của df cho dữ liệu đã được chuẩn hóa
+df_s = pd.DataFrame(df_s, columns=col_names)
+
+print(df_s.head())
+
+# biểu diễn dữ liệu đã được chuẩn hóa
+sns.kdeplot(data=df_s)
+
+# thống kê về dữ liệu được sinh ra
+print(df_s.describe())
+
+# trực quan hóa bằng biểu đồ box plot
+df_s.boxplot()
